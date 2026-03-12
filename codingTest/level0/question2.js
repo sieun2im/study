938ae675458289029d27deeaa985db2612417cb8
sieun2im <kse040531@naver.com>
2026-03-12 20:49:42 +0900

    0312codingTest

---
+++ codingTest/level0/question2.js
@@ -0,0 +1,14 @@
+//정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+function solution(num1, num2) {
+    return num1 % num2;
+}
+
+//정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+function solution(num1, num2) {
+    return num1 * num2;
+}
+
+// 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+function solution(num1, num2) {
+    return Math.trunc(num1 / num2);
+}
