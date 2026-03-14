f47c344e13fc89f0bde01add5a70c5723cd64086
sieun2im <kse040531@naver.com>
2026-03-14 23:33:33 +0900

    codingTest0314

--- codingTest/level0/question2.js
+++ codingTest/level0/question2.js
@@ -61,5 +61,8 @@ function solution(common) {
     }
 }
 
-console.log(solution([1, 2, 3, 4]));
-console.log(solution([2, 4, 8]));
+
+//정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+function solution(numbers) {
+    return numbers.reduce((sum, num) => sum + num) / numbers.length;
+}
