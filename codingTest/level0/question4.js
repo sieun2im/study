301f8284de9b457484e56e4b27569184898d15a3
sieun2im <kse040531@naver.com>
2026-04-03 23:03:52 +0900

    codingTest0403

--- codingTest/level0/question4.js
+++ codingTest/level0/question4.js
@@ -1,5 +1,20 @@
 //두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
 
 function solution(a, b, flag) {
-    return flag ? a+b:a-b;
+    return flag ? a + b : a - b;
+}
+
+//문자들이 담겨있는 배열 arr가 주어집니다. arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+function solution(arr) {
+    return arr.join('');
+}
+
+//문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string의 뒤의 n글자로 이루어진 문자열을 return 하는 solution 함수를 작성해 주세요.
+function solution(my_string, n) {
+    return my_string.slice(my_string.length - n)
+}
+
+//정수 리스트 num_list가 주어질 때, 첫 번째로 나오는 음수의 인덱스를 return하도록 solution 함수를 완성해주세요. 음수가 없다면 -1을 return합니다.
+function solution(num_list) {
+    return num_list.findIndex((i) => i < 0)
 }
