bb68396c1a998175e71fa2c61ffe1a3dbcab9330
sieun2im <kse040531@naver.com>
2026-04-06 23:35:09 +0900

    codingTest0406

--- codingTest/level0/question4.js
+++ codingTest/level0/question4.js
@@ -75,3 +75,21 @@ function solution(n, control) {
 function solution(num, n) {
     return num % n ? 0 : 1;
 }
+
+//두 개의 문자열 str1, str2가 공백으로 구분되어 입력으로 주어집니다.
+//입출력 예와 같이 str1과 str2을 이어서 출력하는 코드를 작성해 보세요.
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    str1 = input[0];
+    str2 = input[1];
+    console.log(str1 + str2);
+});
