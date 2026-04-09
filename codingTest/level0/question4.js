db3148f895e654f9b54943f3562bc0a20cd6a5db
sieun2im <164731772+sieun2im@users.noreply.github.com>
2026-04-09 23:50:09 +0900

    Implement function to remove letter from string
    Add solution function to remove specified letter from string

--- codingTest/level0/question4.js
+++ codingTest/level0/question4.js
@@ -102,3 +102,9 @@ function solution(num_list) {
     answer.push(num_list.filter((n)=>n%2===1).length)
     return answer;
 }
+
+//문자열 my_string과 문자 letter이 매개변수로 주어집니다. my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+function solution(my_string, letter) {
+    var answer = my_string.split(letter).join('');
+    return answer;
+}
