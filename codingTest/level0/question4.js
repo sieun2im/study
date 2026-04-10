46a6137d68be550e4665fcaf9f2e6c81d9f541c0
sieun2im <164731772+sieun2im@users.noreply.github.com>
2026-04-10 23:36:50 +0900

    Add multiple solution functions for coding challenges

--- codingTest/level0/question4.js
+++ codingTest/level0/question4.js
@@ -108,3 +108,19 @@ function solution(my_string, letter) {
     var answer = my_string.split(letter).join('');
     return answer;
 }
+
+//머쓱이는 학교에서 키 순으로 줄을 설 때 몇 번째로 서야 하는지 궁금해졌습니다. 머쓱이네 반 친구들의 키가 담긴 정수 배열 array와 머쓱이의 키 height가 매개변수로 주어질 때, 머쓱이보다 키 큰 사람 수를 return 하도록 solution 함수를 완성해보세요.
+function solution(array, height) {
+   
+    return array.filter((h)=>height<h).length;
+}
+
+//정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+function solution(numbers) {
+    return numbers.map((num)=>num*2);
+}
+
+//머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 주어질 때, 모든 사람이 피자를 한 조각 이상 먹기 위해 필요한 피자의 수를 return 하는 solution 함수를 완성해보세요.
+function solution(n) {
+    return Math.ceil(n/7);
+}
