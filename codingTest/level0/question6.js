c0dad886f5473be24912a7acbddb1545120b5a35
sieun2im <164731772+sieun2im@users.noreply.github.com>
2026-05-18 23:22:09 +0900

    Update question6.js

--- codingTest/level0/question6.js
+++ codingTest/level0/question6.js
@@ -94,3 +94,15 @@ function solution(arr) {
     var answer = arr.reduce((a,b)=>a+b,0);
     return answer/arr.length;
 }
+
+//자연수 n이 매개변수로 주어집니다. n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요. 답이 항상 존재함은 증명될 수 있습니다.
+function solution(n) {
+    var answer = 0;
+    for(let i=0; i<n;i++){
+        if(n%i===1){
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+}
