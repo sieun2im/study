3414dbb269b911f0f97de2758603406f9e8df936
sieun2im <164731772+sieun2im@users.noreply.github.com>
2026-08-23 23:56:46 +0900

    Implement string to number conversion function
    Add solution function to convert string to number

--- codingTest/level0/question6.js
+++ codingTest/level0/question6.js
@@ -106,3 +106,9 @@ function solution(n) {
     }
     return answer;
 }
+
+//문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요
+function solution(s) {
+    var answer = parseInt(s);
+    return answer;
+}
