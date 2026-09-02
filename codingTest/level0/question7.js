6f57bc4444e53e6c8ac33780bc0ac60472adcc7f
sieun2im <164731772+sieun2im@users.noreply.github.com>
2026-09-02 21:53:26 +0900

    Implement string to number conversion function
    Add a function to convert a string with number words to its numeric form.

--- codingTest/level0/question7.js
+++ codingTest/level0/question7.js
@@ -28,3 +28,42 @@ const solution = num_list => {
     })
     return Number(odd)+Number(even);
 }
+
+//두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+
+function solution(a, b) {
+    let answer = 0;
+    for(let i=Math.min(a,b); i<=Math.max(a,b); i++){
+        answer=answer+i
+    }
+    return answer;
+}
+
+/*
+네오와 프로도가 숫자놀이를 하고 있습니다. 네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다.
+
+다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.
+
+1478 → "one4seveneight"
+234567 → "23four5six7"
+10203 → "1zerotwozero3"
+이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다. s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(str) {
+    var answer = str;
+    
+       answer= answer.replaceAll('zero','0');
+       answer= answer.replaceAll('one','1');
+       answer= answer.replaceAll('two','2');
+       answer= answer.replaceAll('three','3');
+       answer= answer.replaceAll('four','4');
+       answer= answer.replaceAll('five','5');
+       answer= answer.replaceAll('six','6');
+       answer= answer.replaceAll('seven','7');
+       answer= answer.replaceAll('eight','8');
+       answer= answer.replaceAll('nine','9');
+    
+
+    return Number(answer);
+}
