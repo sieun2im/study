1e7915e6e00e25c9ce641bf3f5aecd7df7d98fee
sieun2im <164731772+sieun2im@users.noreply.github.com>
2026-09-26 23:54:17 +0900

    Update question7.js

--- codingTest/level0/question7.js
+++ codingTest/level0/question7.js
@@ -480,3 +480,15 @@ function solution(rny_string) {
     var answer = rny_string;
     return answer.replaceAll('m','rn');
 }
+
+/*
+알파벳으로 이루어진 문자열 myString과 pat이 주어집니다. myString의 연속된 부분 문자열 중 pat이 존재하면 1을 그렇지 않으면 0을 return 하는 solution 함수를 완성해 주세요.
+
+단, 알파벳 대문자와 소문자는 구분하지 않습니다.
+*/
+
+function solution(myString, pat) {
+    var answer = myString.toLowerCase();
+    const regex=new RegExp(pat.toLowerCase());
+    return regex.test(answer)?1:0;
+}
